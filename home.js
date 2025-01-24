02097a2dfd3fedd57d4f3dc9b835d89bedbc5823
ariel087 <95010884+ariel087@users.noreply.github.com>
2025-01-25 04:38:49 +0800

    Update home.js

--- home.js
+++ home.js
@@ -15,7 +15,7 @@ export const generateJWTToken = (response, userId) => {
         });
 
         response.cookie('token', token, {
-            httpOnly: false,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'strict',
             maxAge: 24 * 60 * 60 * 1000
